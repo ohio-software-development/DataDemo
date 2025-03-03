f0317ddd3c0ba6c3c1e9d025ed40f51b3edbf4fd
Brady Phelps <bp309420@ohio.edu>
2025-03-02 20:57:55 -0500

    Initial commit

---
+++ StudentDemo.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_absolute_error
+
+df = pd.read_csv('StudentsPerformance.csv')
